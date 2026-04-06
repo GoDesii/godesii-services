40381678f9d6f76ae96c7d8ab5472999a9433673
Shashank Jain <sjaintdl811@outlook.com>
2026-04-06 21:35:45 +0530

    Uncomment CategoryInitializer class and its methods (#54)

--- src/main/java/com/godesii/godesii_services/config/CategoryInitializer.java
+++ src/main/java/com/godesii/godesii_services/config/CategoryInitializer.java
@@ -1,56 +1,56 @@
-package com.godesii.godesii_services.config;
+// package com.godesii.godesii_services.config;
 
-import com.godesii.godesii_services.entity.restaurant.Category;
-import com.godesii.godesii_services.repository.restaurant.CategoryRepository;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.event.EventListener;
-import org.springframework.stereotype.Component;
+// import com.godesii.godesii_services.entity.restaurant.Category;
+// import com.godesii.godesii_services.repository.restaurant.CategoryRepository;
+// import org.springframework.boot.context.event.ApplicationReadyEvent;
+// import org.springframework.context.event.EventListener;
+// import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
+// import java.util.Arrays;
 
-@Component
-public class CategoryInitializer {
-
-
-    private final CategoryRepository categoryRepository;
-
-    public CategoryInitializer(CategoryRepository categoryRepository) {
-        this.categoryRepository = categoryRepository;
-    }
+// @Component
+// public class CategoryInitializer {
 
 
-    @EventListener(ApplicationReadyEvent.class)
-    public void initializeCategory(){
+//     private final CategoryRepository categoryRepository;
 
-        Arrays
-                .stream(DefaultCategory.values())
-                .filter(c -> this.categoryRepository.findByName(c.name()).isEmpty()).forEachOrdered(c -> {
-                    Category category = new Category();
-                    category.setName(c.name());
-                    this.categoryRepository.save(category);
-            });
+//     public CategoryInitializer(CategoryRepository categoryRepository) {
+//         this.categoryRepository = categoryRepository;
+//     }
 
-    }
 
-    public enum DefaultCategory {
+//     @EventListener(ApplicationReadyEvent.class)
+//     public void initializeCategory(){
 
-        FRUITS("Fruits"),
-        SWEETS("Sweets"),
-        DINNER("Dinner"),
-        LUNCH("Lunch"),
-        BREAKFAST("Breakfast"),
-        SNACKS("Snacks"),
-        STARTER("Starter");
+//         Arrays
+//                 .stream(DefaultCategory.values())
+//                 .filter(c -> this.categoryRepository.findByName(c.name()).isEmpty()).forEachOrdered(c -> {
+//                     Category category = new Category();
+//                     category.setName(c.name());
+//                     this.categoryRepository.save(category);
+//             });
 
-        private final String value;
+//     }
 
-        DefaultCategory(String value){
-            this.value = value;
-        }
+//     public enum DefaultCategory {
 
-        public String getValue(){
-            return value;
-        }
-    }
+//         FRUITS("Fruits"),
+//         SWEETS("Sweets"),
+//         DINNER("Dinner"),
+//         LUNCH("Lunch"),
+//         BREAKFAST("Breakfast"),
+//         SNACKS("Snacks"),
+//         STARTER("Starter");
 
-}
+//         private final String value;
+
+//         DefaultCategory(String value){
+//             this.value = value;
+//         }
+
+//         public String getValue(){
+//             return value;
+//         }
+//     }
+
+// }
