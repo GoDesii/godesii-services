681098a7e02b7607bd8bdb0e3d1dcef1c270d38f
Shashank Jain <sjaintdl811@outlook.com>
2026-06-20 16:07:46 +0530

    Feature/villsyn 7 (#89)
    * Added Userservice
    
    * Update villsyn-ac142-firebase-adminsdk-fbsvc-43cc3d2ade.json

--- src/main/java/com/godesii/godesii_services/service/auth/UserService.java
+++ src/main/java/com/godesii/godesii_services/service/auth/UserService.java
@@ -5,7 +5,6 @@ import com.godesii.godesii_services.config.twilio.TwilioSmsSenderService;
 import com.godesii.godesii_services.dto.MobileUserCreationRequest;
 import com.godesii.godesii_services.dto.UserProfileCreateRequest;
 import com.godesii.godesii_services.dto.UserProfileCreateResponse;
-import com.godesii.godesii_services.entity.auth.AuthProvider;
 import com.godesii.godesii_services.entity.auth.Gender;
 import com.godesii.godesii_services.entity.auth.Role;
 import com.godesii.godesii_services.entity.auth.User;
@@ -14,59 +13,57 @@ import com.godesii.godesii_services.repository.auth.UserRepository;
 import com.godesii.godesii_services.security.SecurityUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.InsufficientAuthenticationException;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 
-
-import java.util.HashSet;
 import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 @Service
-public class UserService  {
+public class UserService {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(UserService.class);
 
     private static final String MOBILE_NUMBER_REGEX = "^\\d{10}$"; // Example: 10 digits
 
+    @Value("${spring.profiles.active}")
+    private String profile;
 
     private final UserRepository userRepository;
     private final SecureTokenService secureTokenService;
     private final TwilioSmsSenderService twilioSmsSenderService;
     private final PasswordEncoder passwordEncoder;
 
-    public UserService(UserRepository userRepository, SecureTokenService secureTokenService, TwilioSmsSenderService twilioSmsSenderService, PasswordEncoder passwordEncoder) {
+    public UserService(UserRepository userRepository, SecureTokenService secureTokenService,
+            TwilioSmsSenderService twilioSmsSenderService, PasswordEncoder passwordEncoder) {
         this.userRepository = userRepository;
         this.secureTokenService = secureTokenService;
         this.twilioSmsSenderService = twilioSmsSenderService;
         this.passwordEncoder = passwordEncoder;
     }
 
-    public User createUser(MobileUserCreationRequest request){
+    public User createUser(MobileUserCreationRequest request) {
         this.userRepository.findByUsername("");
         return null;
     }
 
-    public String registerMobileUser(MobileUserCreationRequest request){
-        if(!isMobileNumber(request.getMobile())) {
+    public String registerMobileUser(MobileUserCreationRequest request) {
+        if (!isMobileNumber(request.getMobile())) {
             throw new IllegalArgumentException("");
         }
         String generatedOTP = SecurityUtils.generateOTP();
         User existingUser = checkIfUserExistByMobileNo(request.getMobile());
-        if(existingUser != null){
+        if (existingUser != null) {
             existingUser.setLoginOtp(passwordEncoder.encode(generatedOTP));
             existingUser.setAccountNonExpired(true);
             existingUser.setAccountNonLocked(true);
             existingUser.setCredentialsNonExpired(true);
             existingUser.setEnabled(true);
-            if(!existingUser.getRoles().contains(Role.valueOf(request.getRoleType().toUpperCase()))){
+            if (!existingUser.getRoles().contains(Role.valueOf(request.getRoleType().toUpperCase()))) {
                 existingUser.setRoles(addRoles(existingUser, request.getRoleType()));
             }
             userRepository.saveAndFlush(existingUser);
@@ -83,21 +80,23 @@ public class UserService  {
         user.setAccountNonLocked(true);
         user.setCredentialsNonExpired(true);
         this.userRepository.save(user);
-        LOGGER.info("OTP is generated for mobile no {}", request.getMobile());
-        this.twilioSmsSenderService.sendSms(new SmsRequest(request.getMobile(), "OTP Send! " + generatedOTP));
+        if ("prod".equals(profile)) {
+            LOGGER.info("OTP is generated for mobile no {}", request.getMobile());
+            this.twilioSmsSenderService.sendSms(new SmsRequest(request.getMobile(), "OTP Send! " + generatedOTP));
+            return null;
+        }
         return generatedOTP;
     }
 
-    public User checkIfUserExist(String username){
+    public User checkIfUserExist(String username) {
         return this.userRepository.findByUsername(username).orElse(null);
     }
 
-    public User checkIfUserExistByMobileNo(String mobileNo){
+    public User checkIfUserExistByMobileNo(String mobileNo) {
         return this.userRepository.findByMobileNo(mobileNo).orElse(null);
     }
 
     // You can refine this regex for specific country codes, optional prefixes, etc.
-
     public static boolean isMobileNumber(String username) {
         Pattern pattern = Pattern.compile(MOBILE_NUMBER_REGEX);
         Matcher matcher = pattern.matcher(username);
@@ -108,26 +107,27 @@ public class UserService  {
         return !isMobileNumber(username);
     }
 
-    public UserProfileCreateResponse updateProfile(UserProfileCreateRequest request){
+    public UserProfileCreateResponse updateProfile(UserProfileCreateRequest request) {
         User existingUser = this.userRepository.findByUsername(request.getUserId())
-                .orElseThrow(()-> new ResourceNotFoundException("User does not exist with username " + request.getUserId()));
-        if(StringUtils.hasText(request.getFirstName())){
+                .orElseThrow(() -> new ResourceNotFoundException(
+                        "User does not exist with username " + request.getUserId()));
+        if (StringUtils.hasText(request.getFirstName())) {
             existingUser.setFirstName(request.getFirstName());
         }
-        if(StringUtils.hasText(request.getMiddleName())){
+        if (StringUtils.hasText(request.getMiddleName())) {
             existingUser.setMiddleName(request.getMiddleName());
         }
-        if(StringUtils.hasText(request.getLastName())){
+        if (StringUtils.hasText(request.getLastName())) {
             existingUser.setLastName(request.getLastName());
         }
-        if(StringUtils.hasText(request.getGender())){
+        if (StringUtils.hasText(request.getGender())) {
             existingUser.setGender(Gender.valueOf(request.getGender().toUpperCase()));
         }
         return UserProfileCreateResponse.mapToUserProfileCreateResponse(this.userRepository.save(existingUser));
     }
 
-    private Set<Role> addRoles(User user, String requestedRole){
-        if(Role.ADMIN.name().equalsIgnoreCase(requestedRole) || Role.MANGER.name().equalsIgnoreCase(requestedRole)){
+    private Set<Role> addRoles(User user, String requestedRole) {
+        if (Role.ADMIN.name().equalsIgnoreCase(requestedRole) || Role.MANGER.name().equalsIgnoreCase(requestedRole)) {
             throw new InsufficientAuthenticationException("Authentication Failed!");
         }
         user.getRoles().add(Role.valueOf(requestedRole.toUpperCase()));
